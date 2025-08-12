6798e61609f3e3604aefcdb6fca8ce0f47595d79
EnzoCarrilho <enzocarrilho25@gmail.com>
2025-08-12 18:50:15 -0300

    projeto Finalizado

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 'use strict'
 
 const botaoTrocarImagem = document.getElementById('trocar-imagem')
+const botaoAutomatico = document.getElementById('auto')
 
 function trocarImagem(){
     const imagem = document.getElementById('imagem').value
     document.documentElement.style.setProperty('--imagem','url(./img/'+imagem+'.jpg)')
 }
+
+const imagemContainer = document.querySelector('.box-imagem');
+
+
+let animacaoOn = false
+
+function modoAutomatico(){
+
+    //inverte o estado da variavel animacaoOn para começar desligado
+    animacaoOn = !animacaoOn
+
+    imagemContainer.classList.toggle('animacao-ativa');
+
+   
+        if (imagemContainer.classList.contains('animacao-ativa')) {
+        botaoAutomatico.textContent = 'Parar Modo Automático';
+        } else {
+        botaoAutomatico.textContent = 'Iniciar Modo Automático';
+    }
+}
+
+
+botaoAutomatico.addEventListener('click', modoAutomatico);
 
 botaoTrocarImagem.addEventListener('click',trocarImagem)
